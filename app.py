c75368c4818a2d6a7dc47aaffb331d61eef27f39
Alejo Giraldo <alejito23001@gmail.com>
2021-06-07 18:01:56 -0500

    Se crea inicio de sesion y envio de correo

--- app.py
+++ app.py
@@ -1,2 +1,33 @@
-from flask import Flask
+# ------------------------------ Importación de librerías ------------------------------
+from flask import Flask, request, jsonify, redirect, url_for, render_template
+from werkzeug.utils import redirect
+from envio_correo import confirmar_correo
+
+usuarios = []
+
+# ------------------------------ Configuración ------------------------------
 app = Flask(__name__)
+
+# ------------------------------ Creación de rutas ------------------------------
+# Index
+@app.route('/')
+def index():
+    tittle = "Get It"
+    return "hola"
+
+# Registro
+@app.route('/registro', methods=['POST'])
+def registro():
+    nombre = request.form.get('nombre')
+    email = request.form.get('email')
+    contrasena = request.form.get('contrasena')
+    contrasena2 = request.form.get('contrasena2')
+    if contrasena == contrasena2:
+        confirmar_correo(email)
+        return "200"
+
+@app.route('/login', methods=['GET'])
+def login():
+    return "Ok"
+
+app.run(debug=True, port=5000)
